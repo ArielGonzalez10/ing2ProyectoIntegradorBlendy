fc30e41c4b37ca1912f1b3602e671c12ee5ae938
Ariel Gonzalez <arielgonzalezr9@gmail.com>
2026-03-16 15:32:45 -0300

    Agregado de metodos

--- blendy/src/main/java/com/ing2/blendy/repository/IProductoRepository.java
+++ blendy/src/main/java/com/ing2/blendy/repository/IProductoRepository.java
@@ -6,7 +6,11 @@ package com.ing2.blendy.repository;
 
 import com.ing2.blendy.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  *
@@ -14,5 +18,15 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface IProductoRepository extends JpaRepository<Producto,Integer>{
-    
+    //Busca un producto por el nombre
+    @Query("SELECT p FROM PRODUCTO p WHERE p.descripcion = :p_descripcion")
+    Producto buscarProducto(@Param("p_descripcion") String p_descripcion);
+
+    //Ordena alfabeticamente la lista de productos
+    @Query("SELECT p FROM PRODUCTO p ORDER BY p.descripcion ASC ")
+    List<Producto> filtrarAlfabeticamenteAsc();
+
+    //Ordena alfabeticamente la lista de productos
+    @Query("SELECT p FROM PRODUCTO p ORDER BY p.descripcion Desc ")
+    List<Producto> filtrarAlfabeticamenteDesc();
 }
